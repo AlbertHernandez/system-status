3ef97daefffc65a8c00ded189d7ac93ab3d737b7
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2021-12-27 09:07:21 +0100

    refactor: create method to resolve instances of classes

--- src/apps/backoffice/backend/dependency-injection/shared.dependency.ts
+++ src/apps/backoffice/backend/dependency-injection/shared.dependency.ts
@@ -1,12 +1,11 @@
 import * as Awilix from "awilix";
-import camelcase from "camelcase";
 import { PinoLogger } from "../../../../contexts/shared/infrastructure/pino-logger";
 import { ErrorHandler } from "../../../../contexts/shared/infrastructure/error-handler";
 import { config } from "../../../../contexts/backoffice/shared/infrastructure/config";
-import { Class } from "../../../../contexts/shared/domain/class";
 import { CreateIncidentCommandHandler } from "../../../../contexts/backoffice/incidents/application/create/create-incident-command-handler";
 import { CommandHandlersInformation } from "../../../../contexts/shared/infrastructure/command-bus/in-memory/command-handlers-information";
 import { InMemoryCommandBus } from "../../../../contexts/shared/infrastructure/command-bus/in-memory/in-memory-command-bus";
+import { instancesOf } from "./instances-of";
 
 export const register = (container: Awilix.AwilixContainer) => {
   container.register({
@@ -17,22 +16,8 @@ export const register = (container: Awilix.AwilixContainer) => {
       };
     }),
     errorHandler: Awilix.asClass(ErrorHandler),
-    commandHandlers: Awilix.asFunction(
-      ({
-        parentContainer,
-        handlers,
-      }: {
-        parentContainer: Awilix.AwilixContainer;
-        handlers: Class<unknown>[];
-      }) =>
-        handlers.map((handler) =>
-          parentContainer.resolve(camelcase(handler.name))
-        )
-    ).inject((parentContainer: Awilix.AwilixContainer) => ({
-      parentContainer,
-      handlers: [CreateIncidentCommandHandler],
-    })),
+    commandHandlers: instancesOf([CreateIncidentCommandHandler]),
     commandHandlersInformation: Awilix.asClass(CommandHandlersInformation),
-    commandBus: Awilix.asClass(InMemoryCommandBus),
+    commandBus: Awilix.asClass(InMemoryCommandBus).singleton(),
   });
 };
