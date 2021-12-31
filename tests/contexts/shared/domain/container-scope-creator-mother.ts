6ce5363eb1af2087edd9fb0ec7dfc7cd89d31670
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2021-12-31 12:29:02 +0100

    refactor: simplify types

--- tests/contexts/shared/domain/container-scope-creator-mother.ts
+++ tests/contexts/shared/domain/container-scope-creator-mother.ts
@@ -1,14 +1,9 @@
-import {
-  Container,
-  ContainerScopeCreator,
-} from "../../../../src/contexts/shared/infrastructure/dependency-injection";
+import { ContainerScopeCreator } from "../../../../src/contexts/shared/infrastructure/dependency-injection";
 import { ContainerMother } from "./container-mother";
 
 export class ContainerScopeCreatorMother {
   static create({
     container = ContainerMother.create(),
-  }: {
-    container?: Container;
   } = {}): ContainerScopeCreator {
     return new ContainerScopeCreator({
       container,
